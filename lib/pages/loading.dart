923b82d96da13a878b78af2637633622ca4e0698
bibekupreti9813 <bibekupreti9813@gmail.com>
2021-02-24 17:42:32 +0545

    world time

---
+++ lib/pages/loading.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Kathmandu', flag: 'nepal.svg', url: 'Asia/Kathmandu');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location':instance.location,
+      'flag':instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+   
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitRotatingCircle(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
